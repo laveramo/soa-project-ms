891615750cf3e49efb725e566e94f0b89f0497ac
Vera-Morales Luz-Alejandra <vera-morales@srv-tp05>
2024-11-23 16:13:21 +0100

    add user service

---
+++ userService/src/test/java/fr/insa/userService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
