5fc2be1027e50c17dbe92e0b75ea1ba2ff145410
Oussama <eljidoussama155@gmail.com>
2024-11-23 23:16:58 +0100

    Congig service

---
+++ ConfigServer/src/test/java/f/insa/ms/server/config/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package f.insa.ms.server.config.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
